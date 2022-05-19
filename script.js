4cf5e2e8a77323fea002ad6e8a22b58db58d5f7b
Shihab <73121069+maker-shihab@users.noreply.github.com>
2022-05-19 14:00:20 +0600

    some error fix

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-function handaleSubmmit (event) {
-  event.preventDefault()
-  console.log('Hello');
+function handaleSubmmit(event) {
+  event.preventDefault();
+
+  var answerList = {};
+  var a1a = document.querySelectorAll('.q1a');
+  
+  if (a1a.checked) {
+     answerList.push(a1a.id);
+  } else{
+    console.log('Sorry');
+  }
+  console.log(answerList);
 }
