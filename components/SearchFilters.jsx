49d79c23702278177cf1862cd32ece8ac920a5a8
Yashwant1849 <frontendmaker99@gmail.com>
2023-01-17 02:34:15 +0600

    Update Search Result

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -1,13 +1,48 @@
-// import {useEffect, useState } from "react";
-// import {Flex, Select, Box, Text, Input, Spinner, Icon, Button} from '@chakra-ui/react';
-// import { useRouter } from "next/router";
+import { Box, Flex, Select } from '@chakra-ui/react';
+import { useRouter } from "next/router";
+import { useState } from "react";
 // import {MdCancel} from 'react-icons/md';
+import { filterData, getFilterValues } from '../utils/filterData';
+
 
 const SearchFilters = () => {
+    const [filters, setFilters] = useState(filterData);
+    const router = useRouter();
+
+    const searchProperties = (filterValues) => {
+        const path = router.pathname;
+        const { query } = router;
+
+        const values = getFilterValues(filterValues);
+
+        values.forEach((item) => {
+            query[item.name] = item.value
+
+        })
+        router.push({ pathname: path, query })
+    }
     return(
-        <div>
-            SearchFilters
-        </div>
+        <Flex bg='gray.100' p='4' justifyContent='center' flexWrap='wrap'>
+            {
+                filters.map((filter) => (
+                    <Box key={filter.queryName}>
+                        <Select 
+                        placeholder={filter.placeholder}
+                        w="fit-content"
+                        p="2"
+                        onChange={(e) => searchProperties({[filter.queryName]: e.target.value})}>
+                            {
+                                filter?.items?.map((item) => (
+                                    <option value={item.value} key={item.value}>
+                                        {item.name}
+                                    </option>
+                                ))
+                            }
+                        </Select>
+                    </Box>
+                ))
+            }
+        </Flex>
     )
 };
 
