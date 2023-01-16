6dcf2adf3d5273448903ff67d8ddc8eb07cad803
Yashwant1849 <frontendmaker99@gmail.com>
2023-01-16 22:42:08 +0600

    Add sume advance

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,24 +1,23 @@
 import { Box } from "@chakra-ui/react";
 import Head from "next/head";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
 
-
-const Layout = ({children}) => (
-    <>
-        <Head>
-            <title>Real Estate</title>
-        </Head>
-        <Box maxWidth='1280px' m='auto'>
-            <header>
-                Navbar
-            </header>
-            <main>
-                {children}
-            </main>
-            <footer>
-                Footer
-            </footer>
-        </Box>
-    </>
+const Layout = ({ children }) => (
+  <>
+    <Head>
+      <title>Real Estate</title>
+    </Head>
+    <Box maxWidth="1280px" m="auto">
+      <header>
+        <Navbar />
+      </header>
+      <main>{children}</main>
+      <footer>
+        <Footer/>
+      </footer>
+    </Box>
+  </>
 );
 
 export default Layout;
